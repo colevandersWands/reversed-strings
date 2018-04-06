92f8c0dcfad6060dc5704dcb34386094273a8ca2
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2018-04-06 16:04:23 +0200

    "solution"

--- solution.js
+++ solution.js
@@ -1,11 +1,23 @@
 // Your code here
 
-function reversed_strings(str) {
-  let str_reversed = "";
-  
-  // logic will go here
-  
-  retrun str_reversed;
+function reversed_strings(str)  {
+  if (str.length % 2 == 0) {
+    let length = str.length;
+    for (let i = 0; i <= length/2; i++ ) {
+      let holder = str[i];
+      str[i] = str[length - i]
+      str[length - i] = holder;
+    };
+  } 
+  else {
+    let length = str.length - 1;
+    for (let i = 0; i <= length/2; i++ ) {
+      let holder = str[i];
+      str[i] = str[length - i]
+      str[length - i] = holder;
+    };
+  }
+  return str;
 };
 
 // Be sure it's the cleanest code you can write!
