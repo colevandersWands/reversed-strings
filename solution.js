042625d3e7408b817bb405f93bccdc3901972a95
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2018-04-06 14:52:58 +0200

    empty function

--- solution.js
+++ solution.js
@@ -1,3 +1,11 @@
 // Your code here
 
+function reversed_strings(str) {
+  let str_reversed = "";
+  
+  // logic will go here
+  
+  retrun str_reversed;
+};
+
 // Be sure it's the cleanest code you can write!
